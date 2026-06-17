2354ee7364956635cf6440e7c9f390d5aacf4a2b
yoontae82 <yoontae82@gmail.com>
2026-06-17 15:41:35 +0900

    update

--- js/config.js
+++ js/config.js
@@ -5,7 +5,7 @@
 const API_URL = 'https://script.google.com/macros/s/AKfycbx2kp-Ie2wqL3ErETMTIgWJ4Ko9YQnrpwraVRzt4R6vr2uUbk8NOcfWhx-TP44rB7vd2w/exec';
 
 // 쇼핑몰 이름
-const SHOP_NAME = '오브샬롯';
+const SHOP_NAME = 'OF CHARLOTTE';
 
 // 관리자 비밀번호 (admin.html 접속 시 필요)
 const ADMIN_PASSWORD = 'woahals8802';
